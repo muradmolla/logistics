22c618dfa5f3d6101fa372280eaf2330378cf1ce
muradmolla <muradmollaoglu@gmail.com>
2022-08-12 11:44:09 +0300

    Typography

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,11 +15,13 @@ export default function Navigation() {
     const navRender = sections.map((section: Section) => {
         const activeClasses = scrolledNavItem === section.key ? ' bg-orange' : ''; 
         return (
-            <div className='py-1 px-5 lg:px-0 lg:pl-[3rem] xl:pl-[4rem] w-60 lg:w-auto text-center'>
+            <li className='py-1 px-5 lg:px-0 lg:pl-[3rem] xl:pl-[4rem] w-60 lg:w-auto text-center'>
             <a href='#' className={'py-2 px-8 lg:px-4 rounded-3xl' + activeClasses}>
+                <span>
                 {section.label}
+                </span>
             </a>
-            </div>
+            </li>
             )
     })
 
@@ -27,21 +29,19 @@ export default function Navigation() {
     const navClasses = 'absolute lg:static lg:flex items-center justify-around lg:bg-transparent bg-translucent top-[-2.5rem] right-0 z-10 translate-y-2/4 lg:translate-y-0 py-4'
     const derivedNavClasses = navActive ? navClasses : 'hidden ' + navClasses;
     //ASK: how to solve contact text is closer to the bottom of the div.
-
   return (
-    <div className='fixed w-full py-9'>
+    <header className='fixed w-full py-9'>
         <div className="container mx-auto flex justify-between p-3 sm:p-0">
             <div>
-                <img src={logoUrl} />
+                <img src={logoUrl} alt="logo"/>
             </div>
-            <div className='text-white font-bold flex items-center relative pr-8 lg:pr-0'>
-                <div className={derivedNavClasses}>
+            <nav className='text-white font-bold flex items-center relative pr-8 lg:pr-0'>
+                <ul className={derivedNavClasses}>
                 {navRender}
-                </div>
+                </ul>
                 <div className='lg:hidden' onClick={() => setNavActive(!navActive)}>burger</div>
-            </div>
-            
+            </nav>
         </div>
-    </div>
+    </header>
   )
 }
