c32c3345d61c7c1c6f334ddf0a78350f8cbf99d5
muradmolla <muradmollaoglu@gmail.com>
2022-08-14 14:12:35 +0300

    Contact content fill

---
+++ src/components/Contact.tsx
@@ -0,0 +1,11 @@
+ import React from "react"
+
+ export default function Contact {
+    const section = "contact";
+
+    return (
+        <section id={section} className="bg-blue">
+            <h2></h2>
+        </section> 
+    );
+ }
